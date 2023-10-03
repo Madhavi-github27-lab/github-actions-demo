68dfd7c9d1bb5c1dc18e407ec604e0d9c8d9ca22
Madhavi <m.panati@spglobal.com>
2023-10-03 21:50:24 +0530

    first commit

---
+++ src/test/java/com/dockercicd/DockerCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockercicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
